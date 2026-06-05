9efcfdc081cfede1e148bdbcf0a98129f4cf1bbe
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-05 09:35:31 +0530

    Implement word reversal in a sentence

--- Word_Reversal_Sentence.java
+++ Word_Reversal_Sentence.java
@@ -9,3 +9,46 @@ Example:
 Input: Java is fun
 Output: avaJ si nuf
   */
+package JavaIgniteDay9;
+import java.util.Scanner;
+
+public class Word_Reversal {
+	public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+		
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String str = sc.nextLine();
+
+        String word = "";
+        String res = "";
+
+        for (int i = 0; i < str.length(); i++) 
+        {
+
+            if (str.charAt(i)!=' ')
+            {
+                word += str.charAt(i);
+            }
+            else 
+            {
+                for (int j = word.length() - 1; j >= 0; j--) 
+                {
+                    res += word.charAt(j);
+                }
+
+                res += " ";
+                word = "";
+            }
+        }
+        for (int j = word.length() - 1; j >= 0; j--)
+        {
+            res += word.charAt(j);
+        }
+
+        System.out.println("Reversed String is: \n" + res);
+
+        sc.close();
+    }
+}
+
