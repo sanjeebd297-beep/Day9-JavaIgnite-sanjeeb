a0417663ab87cb94a12cf8992babd39ce228557a
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-05 09:34:04 +0530

    Add string compression functionality
    Implement string compression algorithm to count consecutive characters.

--- String_Compression.java
+++ String_Compression.java
@@ -8,3 +8,35 @@ Example:
 Input: aaabbcc
 Output: a3b2c2
   */
+package JavaIgniteDay9;
+import java.util.Scanner;
+
+public class String_Compression {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string:");
+        String string = sc.nextLine();
+
+        int count = 1;
+        System.out.println("Compressed string:");
+        for(int i = 0; i < string.length(); i++)
+        {
+            while(i < string.length() - 1 &&
+                  string.charAt(i) == string.charAt(i + 1))
+            {
+                count++;
+                i++;
+            }
+
+            System.out.print(string.charAt(i));
+            System.out.print(count);
+
+            count = 1;
+        }
+
+        sc.close();
+    }
+}
