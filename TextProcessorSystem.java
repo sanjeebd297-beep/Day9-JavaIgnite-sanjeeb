97ffe4bd1546330f8111bdc71ef4a7c899dc2593
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-05 09:34:44 +0530

    Implement text processing operations with StringBuilder

--- TextProcessorSystem.java
+++ TextProcessorSystem.java
@@ -20,3 +20,35 @@ Rules:
 Must use StringBuilder only
 Do not convert back to String for operations
 */
+package JavaIgniteDay9;
+import java.util.Scanner;
+
+public class Text_Processor_System {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Original: " + sb);
+        sb.append(" Programming");
+        
+        System.out.println("After Append: " + sb);
+        int mid = sb.length() / 2;
+        sb.insert(mid, " Easy ");
+        
+        System.out.println("After Insert: " + sb);
+        int start = sb.indexOf("Easy");
+        int end = start + 4;
+        sb.replace(start,end, "Powerful");
+        
+        System.out.println("After Replace: " + sb);
+        sb.reverse();
+        System.out.println("After Reverse: " + sb);
+
+        sc.close();
+    }
+}
