5e930b39f0ca06dd7e2c97b917c6f226b39bc677
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-05 09:31:49 +0530

    Implement dynamic string editing operations

--- Dynamic_Editor_System.java
+++ Dynamic_Editor_System.java
@@ -16,3 +16,27 @@ Delete: Java
 Replace: Programming  - Coding
 Reverse final string
 */
+package JavaIgniteDay9;
+
+public class Dynamic_Editor_System {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			StringBuilder sb = new StringBuilder("");
+			System.out.println("Start: "+sb.insert(0,"Java"));
+			System.out.println("Insert: Programming");
+			sb.append(" Programming");
+			System.out.println("String: "+sb);
+			System.out.println("Delete: Java");
+			sb.delete(0,5);
+			System.out.println("String: "+sb);
+			System.out.println("Replace: Programming -> Coding");
+			sb.replace(0, 11, "Coding");
+			System.out.println("String: "+sb);
+			System.out.println("Reverse string: "+sb.reverse());
+			
+		}
+
+	}
+
