235fbeb9acba86c3a70fbeb2ba335c1e286629a7
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-05 09:32:33 +0530

    Implement string case conversion in EmailValidator
    Added functionality to convert input string to upper and lower case.

--- EmailValidator.java
+++ EmailValidator.java
@@ -13,3 +13,21 @@ Output:
 JAVA
 java
 */
+package JavaIgniteDay9;
+import java.util.Scanner;
+
+public class Email_Validator {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter String:");
+			String string = sc.nextLine();
+			
+			System.out.println("Upper case: "+string.toUpperCase());
+			System.out.println("Lower case: "+string.toLowerCase());
+			sc.close();
+		}
+
+	}
